e513157b6582c7381bb3c94444b1d7891cc3ac28
Aaron Smith <aosmith@thebancorp.com>
2017-12-18 11:44:27 -0600

    Initial Commit

---
+++ EFOnTheFly/EFOnTheFly/DataContext.cs
@@ -0,0 +1,7 @@
+﻿using System.Data.Entity;
+
+namespace EFOnTheFly {
+    internal class DataContext<T> : DbContext where T : class {
+        public DbSet<T> Entities { get; set; }
+    }
+}
---
+++ EFOnTheFly/EFOnTheFly/DataRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace EFOnTheFly {
+    public interface IDataRepository<T> {
+        T Get(params object[] keys);
+
+        List<T> Get(Func<T, bool> getFunction);
+
+        List<T> GetByStoredProc(string storedProc, params object[] parameters);
+
+        T Add(T entity);
+
+        T Update(T entity);
+
+        T Delete(T entity);
+    }
+
+    public class DataRepository<T> : IDataRepository<T> where T : class {
+        public T Get(params object[] keys) {
+            T entity;
+
+            using (var context = new DataContext<T>()) {
+                entity = context.Entities.Find(keys);
+            }
+
+            return entity;
+        }
+
+        public List<T> Get(Func<T, bool> getFunction) {
+            List<T> entities;
+
+            using (var context = new DataContext<T>()) {
+                entities = context.Entities.AsNoTracking().Where(getFunction).ToList();
+            }
+
+            return entities;
+        }
+
+        public List<T> GetByStoredProc(string storedProc, params object[] parameters) {
+            using (var context = new DataContext<T>()) {
+                return context.Database.SqlQuery<T>(storedProc, parameters).ToList();
+            }
+        }
+
+        public T Add(T entity) {
+            using (var context = new DataContext<T>()) {
+                context.Entities.Add(entity);
+                context.SaveChanges();
+            }
+
+            return entity;
+        }
+
+        public T Update(T entity) {
+            using (var context = new DataContext<T>()) {
+                context.Entities.Attach(entity);
+                context.Entry(entity).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+
+            return entity;
+        }
+
+        public T Delete(T entity) {
+            using (var context = new DataContext<T>()) {
+                context.Entities.Remove(entity);
+                context.SaveChanges();
+            }
+
+            return entity;
+        }
+    }
+}
