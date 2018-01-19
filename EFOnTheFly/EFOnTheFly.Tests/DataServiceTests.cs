31d002fcbd1326a4871fba0d54e3086e0dbbd112
Aaron Smith <asmitty92@gmail.com>
2018-01-19 16:35:00 -0600

    Configure unit test project with nunit and nsubstitute

--- EFOnTheFly/EFOnTheFly.Tests/DataServiceTests.cs
+++ EFOnTheFly/EFOnTheFly.Tests/DataServiceTests.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
 
-namespace EFOnTheFly.Tests
-{
-    public class DataServiceTests
-    {
+namespace EFOnTheFly.Tests {
+    [TestFixture]
+    public class DataServiceTests {
     }
 }
